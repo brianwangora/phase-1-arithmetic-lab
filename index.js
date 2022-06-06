ca1ff4625461315c97c6206880b4e8f0f69284b4
Brian Wangora <brianwangora@gmail.com>
2022-06-06 15:36:16 +0300

    tests passed except test 2

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = (num1 * num2);
+let random = Math.random(0)
+let num3 = 10;
+let num4 = 6;
+const mod = num3-num4;
+const max = Math.max(1,12,15,9,19,20);
